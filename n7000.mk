0583326b796290c74e6e559b8b792f005057aa77
bauner <bauner@gmx.de>
2014-11-24 10:56:18 +0100

    do not build sensors

--- n7000.mk
+++ n7000.mk
@@ -35,8 +35,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=320
 
 # Sensors
-PRODUCT_PACKAGES += \
-    sensors.exynos4
+# PRODUCT_PACKAGES += \
+#    sensors.exynos4
 
 # Keylayout
 PRODUCT_COPY_FILES += \
